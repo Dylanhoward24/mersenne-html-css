39d6e240f42b70567bbcedd284f944ff797e66b5
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-22 16:14:58 -0500

    finished with lecture for the day

--- events.js
+++ events.js
@@ -7,21 +7,48 @@ $(document).ready(onReady);
 function onReady() {
     console.log('so ready');
     
+    // handle add button events
     // event handler
     // event listener
     // event registration
-    $('#addBtn').on('click', addToList)
+    $('#addBtn').on('click', addToList);
 }
+
+    // handle delete button events
+    console.log('.deleteBtn', $('.deleteBtn'));
+    // $('.deleteBtn').on('click', deleteItem);
+    // this won't work because it isn't there when page loads
+
+    // use a trick to work around this
+    // "event delegation"
+    $(document).on('click', '.deleteBtn', deleteItem);
+
 
 function addToList() {
     console.log('button got clicked');
-    let input = $('input');
-    let inputText = input.val()
-    console.log('inputText', inputText);
 
-    $('ul').append(`
+    // grab the value of my form input
+    let inputValue = $('#aboutYou').val();
+    console.log('inputValue', inputValue);
+
+
+    $('#listOfThings').append(`
         <li>
-            ${input.val()}
+            ${inputValue}
+            <button class="deleteBtn">Delete Me</button>
         </li>
     `);
+
+    // clear my input
+    $('#aboutYou').val('')
+}
+
+function deleteItem(){
+    console.log('in delete item');
+    
+    // this is the button that was clicked
+    console.log('this is', $(this));
+
+    // remove the parent <li> element
+    $(this).parent().remove();
 }
